00158ca778b99ce0f3192b343f089e62ef1976b5
hejsund <155113452+hejsund@users.noreply.github.com>
2025-06-11 16:49:53 +0200

    copy changes in intro

--- src/components/CharlotteIntro.tsx
+++ src/components/CharlotteIntro.tsx
@@ -68,7 +68,7 @@ export const CharlotteIntro = () => {
               </p>
               
               <p>
-                <span className="text-coral font-medium">Inga pekpinne, inga förbud</span> – bara verktyg som hjälper dig att skapa den sommar DU vill ha.
+                <span className="text-coral font-medium">Inga pekpinnar, inga förbud</span> – bara verktyg som hjälper dig att skapa den sommar DU vill ha.
               </p>
             </div>
 
