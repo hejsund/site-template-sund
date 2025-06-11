978dbe16abf3050f3a51261a673fc762584ea638
hejsund <155113452+hejsund@users.noreply.github.com>
2025-06-11 15:06:49 +0200

    video

--- src/components/VideoModal.tsx
+++ src/components/VideoModal.tsx
@@ -15,7 +15,7 @@ export const VideoModal = ({ isOpen, onClose }: VideoModalProps) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogOverlay className="bg-black/50 backdrop-blur-sm" />
-      <DialogContent className="max-w-4xl w-[90vw] h-auto p-0 border-none bg-transparent shadow-none">
+      <DialogContent className="max-w-sm sm:max-w-md w-[90vw] sm:w-[400px] h-auto p-0 border-none bg-transparent shadow-none">
         <div className="relative w-full">
           {/* Close button */}
           <button
@@ -26,14 +26,13 @@ export const VideoModal = ({ isOpen, onClose }: VideoModalProps) => {
             <X className="w-6 h-6" />
           </button>
           
-          {/* Video container - rotated 90 degrees right */}
+          {/* Video container - correct portrait ratio */}
           <div className="w-full bg-black rounded-lg overflow-hidden">
-            <div className="relative w-full" style={{ paddingTop: '56.25%' }}>
+            <div className="relative w-full" style={{ paddingTop: '177.77777777777777%' }}>
               <iframe
                 src="https://customer-cb54xmjlmqvsgehg.cloudflarestream.com/26a0dbe82ebc64692ab0d64b9473b884/iframe?preload=true&autoplay=true&poster=https%3A%2F%2Fcustomer-cb54xmjlmqvsgehg.cloudflarestream.com%2F26a0dbe82ebc64692ab0d64b9473b884%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
                 loading="lazy"
                 className="absolute top-0 left-0 w-full h-full border-none"
-                style={{ transform: 'rotate(90deg)' }}
                 allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
                 allowFullScreen
                 title="Charlotte välkomnar dig till Sommarboosten"
