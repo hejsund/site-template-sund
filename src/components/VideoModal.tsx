51e51b5bb11c41d1b7577870bebd3553540b7dea
hejsund <155113452+hejsund@users.noreply.github.com>
2025-06-11 15:11:02 +0200

    portrait

--- src/components/VideoModal.tsx
+++ src/components/VideoModal.tsx
@@ -26,11 +26,11 @@ export const VideoModal = ({ isOpen, onClose }: VideoModalProps) => {
             <X className="w-6 h-6" />
           </button>
           
-          {/* Video container - correct portrait ratio */}
+          {/* Video container - portrait with orientation parameter */}
           <div className="w-full bg-black rounded-lg overflow-hidden">
             <div className="relative w-full" style={{ paddingTop: '177.77777777777777%' }}>
               <iframe
-                src="https://customer-cb54xmjlmqvsgehg.cloudflarestream.com/26a0dbe82ebc64692ab0d64b9473b884/iframe?preload=true&autoplay=true&poster=https%3A%2F%2Fcustomer-cb54xmjlmqvsgehg.cloudflarestream.com%2F26a0dbe82ebc64692ab0d64b9473b884%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+                src="https://customer-cb54xmjlmqvsgehg.cloudflarestream.com/26a0dbe82ebc64692ab0d64b9473b884/iframe?preload=true&autoplay=true&orientation=portrait&poster=https%3A%2F%2Fcustomer-cb54xmjlmqvsgehg.cloudflarestream.com%2F26a0dbe82ebc64692ab0d64b9473b884%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
                 loading="lazy"
                 className="absolute top-0 left-0 w-full h-full border-none"
                 allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
